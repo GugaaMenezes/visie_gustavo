161f7692a8087bb0f50e66c555746c44effcac4c
Gustavo Menezes <gustavo.menezes@live.com>
2023-11-08 00:43:26 -0300

    feat: Add api endpoints for CRUD app

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,9 +1,21 @@
-from flask import Blueprint, redirect, url_for
+from app import db
+from flask import Blueprint
+
+from app.controller import persons
 
 
-api = Blueprint('api', __name__)
+api = Blueprint("api", __name__)
 
+session = db.session
 
-@api.route("/", methods=['GET','POST'])
-def login():
-    return "Hello World!"
+@api.route("/", methods=["GET","POST"])
+def index():
+    return "Welcome a my mini app - Created by Gustavo Menezes!"
+
+@api.route("/pessoas", methods=["GET","POST"])
+def person():
+    return persons.person()
+
+@api.route("/pessoas/<int:id_pessoa>", methods=["PUT","DELETE"])
+def data_person(id_pessoa):
+    return persons.data_person(id_pessoa)
