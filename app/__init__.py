9b2e3fffbc744815557e396d8b97c1edc2825ca1
Gustavo Menezes <gustavo.menezes@live.com>
2023-11-08 12:28:55 -0300

    feat: Add routes web and CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+CORS(app)
 
-from .routes import routes
-from .routes.routes import api
+from app.views.home import home_blueprint
+
+from app.routes import routes
+from app.routes.api_routes import api
 
 app.register_blueprint(api, url_prefix='/api')
+
+app.register_blueprint(home_blueprint)
